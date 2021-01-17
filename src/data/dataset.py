7dea4660cd2ea5fa96a5ec3ff4d5d294bcf0f426
Sayar1106 <sayarbanerjee@gmail.com>
2021-01-17 15:45:34 +0530

    Adding dataset and predict

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+import os
 from pathlib import Path
 import click
 import numpy as np
@@ -62,25 +63,28 @@ class TestClassificationDataset:
     def __init__(self, image_paths, resize, augmentations=None):
         self.image_paths = image_paths
         self.aug = augmentations
+        self.resize = resize
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, item):
+        # TODO(Sayar) Handle parent var better
+        parent = "/Users/Banner/Downloads"
         img_id = self.image_paths[item]
-        image = Image.open(img_id)
+        image = Image.open(os.path.join(parent, img_id))
         image = image.convert("RGB")
         if self.resize is not None:
             image = image.resize(
                 (self.resize[1], self.resize[0]), resample=Image.BILINEAR
             )
         image = np.array(image)
-        if self.augmentations is not None:
-            augmented = self.augmentations(image=image)
+        if self.aug is not None:
+            augmented = self.aug(image=image)
             image = augmented["image"]
         image = np.transpose(image, (2, 0, 1)).astype(np.float32)
 
-        return {"image": torch.tensor(image), "image_id": img_id}
+        return {"image": torch.tensor(image, dtype=torch.float), "image_id": img_id}
 
 
 class ClassificationDataLoader:
--- src/predict.py
+++ src/predict.py
@@ -1,5 +1,6 @@
 import glob
-from src.train_model import fetch_env_dict
+import os
+import ast
 import torch
 import albumentations
 import pandas as pd
@@ -9,20 +10,45 @@ from PIL import Image
 import joblib
 import torch.nn as nn
 from torch.nn import functional as F
-from data.dataset import TestBengaliClassificationDataset
+import albumentations as A
+from data.dataset import TestClassificationDataset
 from models.model import ResNet18
 
-env_dict = fetch_env_dict()
+
+def fetch_env_dict():
+    env_dict = {}
+    env_dict["DEVICE"] = os.environ.get("DEVICE")
+    env_dict["IMG_HEIGHT"] = int(os.environ.get("IMG_HEIGHT"))
+    env_dict["IMG_WIDTH"] = int(os.environ.get("IMG_WIDTH"))
+    env_dict["TEST_BATCH_SIZE"] = int(os.environ.get("TEST_BATCH_SIZE"))
+    env_dict["MODEL_MEAN"] = ast.literal_eval(os.environ.get("MODEL_MEAN"))
+    env_dict["MODEL_STD"] = ast.literal_eval(os.environ.get("MODEL_STD"))
+
+    return env_dict
 
 
-def model_predict(model):
+def model_predict(model, env_dict):
     c_pred, p_pred, s_pred, l_pred, n_pred, m_pred, f_pred = [], [], [], [], [], [], []
     img_ids_list = []
 
+    # TODO(Sayar) Handle path better
     df = pd.read_csv("/Users/Banner/Downloads/test.csv")
 
-    dataset = TestBengaliClassificationDataset(
-        image_paths, targets, resize, augmentations
+    aug = A.Compose(
+        [
+            A.Normalize(
+                env_dict["MODEL_MEAN"],
+                env_dict["MODEL_STD"],
+                max_pixel_value=255.0,
+                always_apply=True,
+            )
+        ]
+    )
+
+    dataset = TestClassificationDataset(
+        image_paths=df["img_path"].tolist(),
+        resize=(env_dict["IMG_HEIGHT"], env_dict["IMG_WIDTH"]),
+        augmentations=aug,
     )
     data_loader = torch.utils.data.DataLoader(
         dataset=dataset,
@@ -33,8 +59,8 @@ def model_predict(model):
 
     for bi, d in enumerate(data_loader):
         image = d["image"]
-        img_id = d["img_id"]
-        image = image.to(env_dict["DEVICE"], type=torch.float)
+        img_id = d["image_id"]
+        image = image.to(env_dict["DEVICE"], dtype=torch.float)
         categories, pattern, sleeve, length, neckline, material, fit = model(image)
 
         for ii, imid in enumerate(img_id):
@@ -52,6 +78,7 @@ def model_predict(model):
 
 def main():
     model = ResNet18(pretrained=False)
+    env_dict = fetch_env_dict()
     final_c_pred = []
     final_p_pred = []
     final_s_pred = []
@@ -62,8 +89,14 @@ def main():
     final_img_ids = []
 
     for i in range(5):
-        model.load_state_dict(torch.load(f"../models/resnet18_fold{i}.bin"))
-        model.to(fetch_env_dict["DEVICE"])
+        # TODO(Sayar): Handle CUDA vs CPU flag
+        model.load_state_dict(
+            torch.load(
+                f"../models/resnet18_fold{i}.bin",
+                map_location=torch.device(env_dict["DEVICE"]),
+            )
+        )
+        model.to(env_dict["DEVICE"])
         model.eval()
         (
             c_pred,
@@ -74,7 +107,7 @@ def main():
             m_pred,
             f_pred,
             img_ids_list,
-        ) = model_predict()
+        ) = model_predict(model, env_dict)
         final_c_pred.append(c_pred)
         final_p_pred.append(p_pred)
         final_s_pred.append(s_pred)
@@ -84,23 +117,28 @@ def main():
         final_f_pred.append(f_pred)
         if i == 0:
             final_img_ids.extend(img_ids_list)
-        final_c = np.argmax(np.mean(np.array(final_c_pred), axis=0), axis=1)
-        final_p = np.argmax(np.mean(np.array(final_p_pred), axis=0), axis=1)
-        final_s = np.argmax(np.mean(np.array(final_s_pred), axis=0), axis=1)
-        final_l = np.argmax(np.mean(np.array(final_l_pred), axis=0), axis=1)
-        final_n = np.argmax(np.mean(np.array(final_n_pred), axis=0), axis=1)
-        final_m = np.argmax(np.mean(np.array(final_m_pred), axis=0), axis=1)
-        final_f = np.argmax(np.mean(np.array(final_f_pred), axis=0), axis=1)
+
+    final_c = np.argmax(np.mean(np.array(final_c_pred), axis=0), axis=1)
+    final_p = np.argmax(np.mean(np.array(final_p_pred), axis=0), axis=1)
+    final_s = np.argmax(np.mean(np.array(final_s_pred), axis=0), axis=1)
+    final_l = np.argmax(np.mean(np.array(final_l_pred), axis=0), axis=1)
+    final_n = np.argmax(np.mean(np.array(final_n_pred), axis=0), axis=1)
+    final_m = np.argmax(np.mean(np.array(final_m_pred), axis=0), axis=1)
+    final_f = np.argmax(np.mean(np.array(final_f_pred), axis=0), axis=1)
 
     predictions = []
     for ii, imid in enumerate(final_img_ids):
-        predictions.append((f"{imid}_category", final_c))
-        predictions.append((f"{imid}_pattern", final_p))
-        predictions.append((f"{imid}_sleeve", final_s))
-        predictions.append((f"{imid}_length", final_l))
-        predictions.append((f"{imid}_neckline", final_n))
-        predictions.append((f"{imid}_material", final_m))
-        predictions.append((f"{imid}_fit", final_f))
+        predictions.append((f"{imid}_fit", final_c[ii]))
+        predictions.append((f"{imid}_pattern", final_p[ii]))
+        predictions.append((f"{imid}_sleeve", final_s[ii]))
+        predictions.append((f"{imid}_length", final_l[ii]))
+        predictions.append((f"{imid}_neckline", final_n[ii]))
+        predictions.append((f"{imid}_material", final_m[ii]))
+        predictions.append((f"{imid}_category", final_f[ii]))
 
     submission = pd.DataFrame(predictions, columns=["row_id", "target"])
-    submission.to_csv("submission", index=False)
+    submission.to_csv("submission.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
